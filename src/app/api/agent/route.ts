f48bbdefec7bd39e136b32b9f05a1992bc4aeebc
Subhan-Asghar <subhanasghar16@gmail.com>
2025-06-05 21:48:06 +0500

    add tool

--- src/app/api/agent/route.ts
+++ src/app/api/agent/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse,NextRequest } from "next/server";
 import { createGroq } from '@ai-sdk/groq';
 import { generateText } from 'ai';
-
+import { latest_mail } from "@/tools/tools";
 const groq = createGroq({
     apiKey:process.env.GROQ_API_KEY
   });
@@ -11,16 +11,16 @@ const model = groq('gemma2-9b-it');
 export async function POST(req:NextRequest){
     const {prompt} =await req.json()
 
-    const result= await generateText({
+    const agent_result= await generateText({
         model:model,
         tools:{
-
+            latest_mail
         },
         prompt: prompt
     })
 
     return NextResponse.json({
-        response:result.text,
+        response:agent_result.toolResults
     })
 
 }
