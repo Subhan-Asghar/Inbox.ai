9adff9e80b21ae236b1c7163666454261e7ecc2f
Subhan-Asghar <subhanasghar16@gmail.com>
2025-06-16 22:26:51 +0500

    Change the prompt of the agent

--- src/app/api/agent/route.ts
+++ src/app/api/agent/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import { createGroq } from '@ai-sdk/groq';
 import { generateText } from 'ai';
-import { latest_mail,Sent_mail,heelo_subhan,get_mail_date } from "@/tools/tools";
+import { latest_mail,Sent_mail,heelo_subhan,get_mail_date,getMailAddress } from "@/tools/tools";
 import { refreshToken } from "@/lib/refreshToken";
 
 const groq = createGroq({
@@ -16,24 +16,29 @@ export async function POST(req: NextRequest) {
 
   const agent_result = await generateText({
     model: model,
-    prompt: `You're an assistant that helps with emails and also responds conversationally. Only use tools if the user clearly asks for email-related info. Otherwise, just reply directly.\n\nUser: ${prompt}`,
+    prompt: `You're an assistant that helps with emails and also responds conversationally. Otherwise, just reply directly.\n\nUser: ${prompt}`,
     tools: {
-      heelo_subhan,
+      // heelo_subhan,
       latest_mail,
       Sent_mail,
-      get_mail_date
+      get_mail_date,
+      getMailAddress
     },
     toolChoice: "auto",
   });
 
-    const toolResult = agent_result.toolResults[0].result;
+    const toolResult = agent_result.toolResults[0]?.result || false;
+    if(toolResult){
       const refine_agent=await generateText({
         model:model,
-        prompt: `Convert the following JSON into a human-readable message:\n\n${JSON.stringify(toolResult, null, 2)}`,          })
+        prompt: `Convert the following JSON into a human-readable message and dont add extra text just make it look good:\n\n${JSON.stringify(toolResult, null, 2)}`,          })
       return NextResponse.json({
         response: refine_agent.text
       });
-     
+    }
+     return NextResponse.json({
+      response:agent_result.text
+     })
 
 
     }
